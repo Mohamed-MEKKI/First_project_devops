0f52cd878dd0611acf0349947d9b26d0c9bb68c8
Mohamed-MEKKI <94528286+Mohamed-MEKKI@users.noreply.github.com>
2022-10-08 13:56:28 +0200

    Update user.js

--- lab3/src/routes/user.js
+++ lab3/src/routes/user.js
@@ -21,9 +21,13 @@ userRouter
       resp.status(201).json(respObj)
     })
   })
-  // .get('/:username', (req, resp, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
-  //   // TODO Create get method API
-  //   const username = req.params.username
+ .get('/:username', (req, resp, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
+   // TODO Create get method API
+     console.log('the response will be sent by the next function ...')
+  next()
+}, (req, res) => {
+  const username = req.params.username
+  res.send(username)
   // })
   
 module.exports = userRouter
